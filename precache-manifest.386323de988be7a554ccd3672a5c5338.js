e696143205bfa64e45b856c1773cb82ff5cff18f
kapilan98 <kapilansamaraj@gmail.com>
2020-07-13 14:29:36 +0530

    Updates

---
+++ precache-manifest.386323de988be7a554ccd3672a5c5338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb9e6f682291a8cdc758b68a35065f7",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "b121663ce427d51168d9",
+    "url": "/covid-19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0029d1962504150bc32",
+    "url": "/covid-19/static/css/main.b6cffec2.chunk.css"
+  },
+  {
+    "revision": "b121663ce427d51168d9",
+    "url": "/covid-19/static/js/2.21bd165a.chunk.js"
+  },
+  {
+    "revision": "6d94984a5fdbad217098935a3d45084e",
+    "url": "/covid-19/static/js/2.21bd165a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0029d1962504150bc32",
+    "url": "/covid-19/static/js/main.e67d0808.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  }
+]);
